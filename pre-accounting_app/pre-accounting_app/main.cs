464416c889650299c1f2c0b60eb6b844b4399a94
bugra-sarikaya <bugra_sarikaya@windowslive.com>
2022-08-17 11:55:29 +0300

    Startup files were added.

---
+++ pre-accounting_app/pre-accounting_app/main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pre_accounting_app {
+    internal class main {
+        static void Main(string[] args) {
+
+        }
+    }
+}
